82fee483e6a79474c3b1f68ccf22ae3921a8ac1f
Banyazavi <banyazavi.choi@gmail.com>
2022-05-14 18:42:56 +0900

    Initialize the Spring Boot project

---
+++ src/main/kotlin/com/banyazavi/pokeapidc/PokeapiDcApplication.kt
@@ -0,0 +1,11 @@
+package com.banyazavi.pokeapidc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PokeapiDcApplication
+
+fun main(args: Array<String>) {
+    runApplication<PokeapiDcApplication>(*args)
+}
---
+++ src/test/kotlin/com/banyazavi/pokeapidc/PokeapiDcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.banyazavi.pokeapidc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PokeapiDcApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
